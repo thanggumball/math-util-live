1b39d722cceab47840be180e5623850a1df55634
thanggumball <thanglatoiii@gmail.com>
2024-04-22 20:55:21 +0700

    Lan dau lam chuyen ay

---
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fu.mathutil;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class MathUtilLive {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        long result = MathUtil.getFactorial(5);
+        
+        System.out.println("5! - 120; actual is " + result);
+        System.out.println("6! - 720; actual is " + MathUtil.getFactorial(6));
+        System.out.println("0! - 1; actual is " + MathUtil.getFactorial(0));
+    }
+    
+}
